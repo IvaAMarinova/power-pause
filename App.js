48712a11d66a2f9c6fa67c3fff78fce08f5d5fe1
Viktor Velkov <20viko06@gmail.com>
2023-04-01 19:57:12 +0200

    register page

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,11 @@ export default function App() {
           name="LoginScreen"
           component={LoginScreen}
           options={{ title: "Login" }}
+        />
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{ title: "Register", headerBackVisible: false }}
         />
         <Stack.Screen
           name="HomeScreen"
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,62 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import FlatButton from "../elements/button";
+import FlatTextInput from "../elements/textinput";
+
+export default function RegisterScreen({ navigation }) {
+  const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/logo.png")}
+        style={{
+          width: 300,
+          height: 300,
+          marginBottom: 40,
+        }}
+      />
+      <FlatTextInput
+        placeholder="First Name"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+      <FlatTextInput
+        placeholder="Last Name"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+      <FlatTextInput
+        placeholder="Email"
+        onChangeText={(newText) => setText(newText)}
+        defaultValue={text}
+      />
+      <FlatTextInput
+        secureTextEntry={true}
+        placeholder="Password"
+        onChangeText={(newPassword) => setPassword(newPassword)}
+        defaultValue={password}
+      />
+      <FlatButton
+        text="Register"
+        onPress={() => navigation.navigate("HomeScreen")}
+      />
+      <Text onPress={() => navigation.pop()}>
+        Already have an account?
+      </Text>
+      <Text>{text}</Text>
+      <Text>{password}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
