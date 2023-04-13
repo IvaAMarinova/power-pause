ada2d7529975043198de07cff9c9932ab7befdd8
Viktor Velkov <20viko06@gmail.com>
2023-04-13 23:20:58 +0300

    for merge

---
+++ backend/MySQLConnector.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "sql7.freemysqlhosting.net",
+  user: "sql7612211",
+  password: "HWby1NKrxf",
+  database: "sql7612211",
+});
